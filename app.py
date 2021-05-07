288db13e69bd7fb954b3ae8fdecb687aa686238f
Dan Sinclair <dan.sinclair@poqcommerce.com>
2021-05-07 22:03:19 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,149 @@
+import os
+from flask import Flask, request, jsonify
+from sqlite3 import connect as sqlconn
+
+try:
+    import Server
+    DB_TIMEOUT = Server.DB_TIMEOUT
+    CRYPTO_DATABASE = Server.DATABASE
+    TRANSACTIONS_DATABASE = Server.CONFIG_TRANSACTIONS
+
+except:
+    DB_TIMEOUT = 10
+    CONFIG_BASE_DIR = "../duco-rest-api-config"
+    CRYPTO_DATABASE = os.path.join(CONFIG_BASE_DIR, 'crypto_database.db')
+    TRANSACTIONS_DATABASE = os.path.join(CONFIG_BASE_DIR, "transactions.db")
+
+app = Flask(__name__)
+
+
+
+#                                                  #
+# =================== BALANCES =================== #
+#                                                  #
+
+def _row_to_balance(row):
+    return {
+        'username': str(row[0]),
+        'balance': float(row[3])
+    }
+
+@app.route('/balances', methods=['GET'])
+def all_balances():
+    balances = []
+    with sqlconn(CRYPTO_DATABASE, timeout=DB_TIMEOUT) as conn:
+        datab = conn.cursor()
+        datab.execute(
+            """SELECT *
+            FROM Users
+            ORDER BY balance DESC""")
+        for row in datab.fetchall():
+            if float(row[3]) > 0:
+                balances.append(_row_to_balance(row))
+            else:
+                # Stop when rest of the balances are just empty accounts
+                break
+
+    return jsonify(balances)
+
+@app.route('/balances/<username>', methods=['GET'])
+def user_balances(username):
+    balance = {}
+    with sqlconn(CRYPTO_DATABASE, timeout=DB_TIMEOUT) as conn:
+        datab = conn.cursor()
+        datab.execute(
+            """SELECT *
+            FROM Users
+            WHERE username = ?
+            ORDER BY balance DESC""", (username,))
+        row = datab.fetchone()
+        if row:
+            balance = _row_to_balance(row)
+
+    return jsonify(balance)
+
+
+
+#                                                      #
+# =================== TRANSACTIONS =================== #
+#                                                      #
+
+def _row_to_transaction(row):
+    return {
+        'datetime': str(row[0]),
+        'sender': str(row[1]),
+        'recipient': str(row[2]),
+        'amount': float(row[3]),
+        'hash': str(row[4]),
+        'memo': str(row[5])
+    }    
+
+@app.route('/transactions', methods=['GET'])
+def all_transactions():
+    transactions = []
+    with sqlconn(TRANSACTIONS_DATABASE, timeout=DB_TIMEOUT) as conn:
+        datab = conn.cursor()
+        datab.execute("SELECT * FROM Transactions")
+        
+        transactions = [_row_to_transaction(row) for row in datab.fetchall()]
+        
+    return jsonify(transactions)
+
+@app.route('/transactions/<username>', methods=['GET'])
+def user_transactions(username):
+    transactions = []
+    with sqlconn(TRANSACTIONS_DATABASE, timeout=DB_TIMEOUT) as conn:
+        datab = conn.cursor()
+        datab.execute(
+            """SELECT * FROM Transactions 
+            WHERE username = ? OR 
+            recipient = ?""", (username, username))
+        
+        transactions = [_row_to_transaction(row) for row in datab.fetchall()]
+        
+    return jsonify(transactions)
+
+@app.route('/transactions/<sender>/to/<recipient>', methods=['GET'])
+def transactions_from_to(sender, recipient):
+    transactions = []
+    with sqlconn(TRANSACTIONS_DATABASE, timeout=DB_TIMEOUT) as conn:
+        datab = conn.cursor()
+        datab.execute(
+            """SELECT * FROM Transactions 
+            WHERE username = ? AND 
+            recipient = ?""", (sender, recipient))
+        
+        transactions = [_row_to_transaction(row) for row in datab.fetchall()]
+        
+    return jsonify(transactions)
+
+@app.route('/transactions/to/<recipient>', methods=['GET'])
+def transactions_to(recipient):
+    transactions = []
+    with sqlconn(TRANSACTIONS_DATABASE, timeout=DB_TIMEOUT) as conn:
+        datab = conn.cursor()
+        datab.execute(
+            """SELECT * FROM Transactions 
+            WHERE recipient = ?""", (recipient,))
+        
+        transactions = [_row_to_transaction(row) for row in datab.fetchall()]
+        
+    return jsonify(transactions)
+
+@app.route('/transactions/from/<sender>', methods=['GET'])
+def transactions_from(sender):
+    transactions = []
+    with sqlconn(TRANSACTIONS_DATABASE, timeout=DB_TIMEOUT) as conn:
+        datab = conn.cursor()
+        datab.execute(
+            """SELECT * FROM Transactions 
+            WHERE username = ?""", (sender,))
+        
+        transactions = [_row_to_transaction(row) for row in datab.fetchall()]
+        
+    return jsonify(transactions)
+
+
+
+if __name__ == '__main__':
+    app.run()
