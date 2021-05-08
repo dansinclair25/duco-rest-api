73dad87e5b39187e2617ba3a4151b8de26d39f66
Dan Sinclair <dansinclair@me.com>
2021-05-08 12:59:22 +0100

    Add miners and API endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from flask import Flask, request, jsonify
 from sqlite3 import connect as sqlconn
 
@@ -7,12 +7,18 @@ try:
     DB_TIMEOUT = Server.DB_TIMEOUT
     CRYPTO_DATABASE = Server.DATABASE
     TRANSACTIONS_DATABASE = Server.CONFIG_TRANSACTIONS
+    MINERS = Server.minerapi.copy()
+    API_JSON_URI = 'api.json'
 
 except:
     DB_TIMEOUT = 10
     CONFIG_BASE_DIR = "../duco-rest-api-config"
     CRYPTO_DATABASE = os.path.join(CONFIG_BASE_DIR, 'crypto_database.db')
     TRANSACTIONS_DATABASE = os.path.join(CONFIG_BASE_DIR, "transactions.db")
+    with open(os.path.join(CONFIG_BASE_DIR, 'miners.json'), 'r') as f:
+        MINERS = json.load(f)
+    API_JSON_URI = os.path.join(CONFIG_BASE_DIR, 'api.json')
+
 
 app = Flask(__name__)
 
@@ -142,6 +148,47 @@ def transactions_from(sender):
         transactions = [_row_to_transaction(row) for row in datab.fetchall()]
         
     return jsonify(transactions)
+
+
+
+#                                                #
+# =================== MINERS =================== #
+#                                                #
+
+def _get_miners():
+    miners = []
+    for k, v in MINERS.items():
+        miner = {'id': k}
+        for subK, subV in v.items():
+            miner[str(subK).lower()] = subV
+
+        miners.append(miner)
+    
+    return miners
+
+@app.route('/miners', methods=['GET'])
+def all_miners():
+    return jsonify(_get_miners())
+
+
+@app.route('/miners/<username>', methods=['GET'])
+def user_miners(username):
+    miners = [m for m in _get_miners() if m['user'] == username]
+    return jsonify(miners)
+
+
+
+#                                             #
+# =================== API =================== #
+#                                             #
+
+def _get_api_data():
+    with open(API_JSON_URI, 'r') as f:
+        return json.load(f)
+
+@app.route('/', methods=['GET'])
+def get_api_data():
+    return jsonify(_get_api_data())
 
 
 
